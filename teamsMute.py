b20b1259bec93e6178e6cef0962fd3deb594bc03
DanValnicek <danvalnicek@gmail.com>
2020-10-27 17:33:05 +0100

    remake of the first version with better optimization

--- teamsMute.py
+++ teamsMute.py
@@ -2,29 +2,18 @@
 
 import subprocess,re
  
-#Listing possible PIDs of MS Teams
-#p = process
-p = subprocess.Popen("xdotool search \'Teams$\'", stdout=subprocess.PIPE, shell=True)
-(output, err) = p.communicate()
- 
-#converting output to string and cutting unnecesary parts 
-#we end up left with clen list of possible pids
-output = str(output)
-output = output.replace('\'','')
-output = output.replace('b','')
-pids = output.split("\\n",)
-del pids[-1]
-print("Possible pids : ", pids)
-
-for pid in pids :
-
-    #looking for window that hasn't parameter _NET_WM_STATE_SKIP_TASKBAR
-    p = subprocess.Popen("xprop -id "+ pid +"|grep _NET_WM_STATE_SKIP_TASKBAR", stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-    output = str(output)
-    output = output.replace('\'','')
-    output = output.replace('b','')
-    
-    #if there is parameter _NET_WM_STATE_SKIP_TASKBAR it means that teams is closed so we don't try to send ctrl+shift+m to it
-    if output == '':
-        subprocess.Popen("xdotool windowactivate --sync "+ pid +" key Ctrl+Shift+M", stdout=subprocess.PIPE, shell=True)
+#finds current name of MS Teams
+name = subprocess.Popen("wmctrl -l| grep 'Microsoft Teams$'|awk '{print substr($0, index($0,$4))  }'",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+print(name)
+#if it has (Meeting) in name script is run, otherwise we're no in meeting so there is no need to do this
+if "(Meeting)" in name:
+#save pid of current window
+    currentlyActive = subprocess.Popen("xdotool getactivewindow",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+    print(currentlyActive)
+#finding pid of MS Teams    
+    pid = subprocess.Popen("xdotool search --name '" + name +"'", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
+    print(pid)
+#facusing and sending CTRL + SHIFT + M    
+    subprocess.Popen("xdotool windowactivate "+ pid +" key Ctrl+Shift+M", stdout=subprocess.PIPE, shell=True)
+#focusing windows that was previously active    
+    subprocess.Popen("xdotool windowactivate '" + currentlyActive + "'", stdout=subprocess.PIPE, shell=True)

c26f371e9865cecfd3abcac1fe7e11bd42aee5d9
DanValnicek <danvalnicek@gmail.com>
2020-10-27 18:58:31 +0100

    comment typo fix

--- teamsMute.py
+++ teamsMute.py
@@ -6,11 +6,11 @@ import subprocess,re
 name = subprocess.Popen("wmctrl -l| grep 'Microsoft Teams$'|awk '{print substr($0, index($0,$4))  }'",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
 #if it has (Meeting) in name script is run, otherwise we're no in meeting so there is no need to do this
 if "(Meeting)" in name:
-#save pid of current window
+#saving pid of the current window
     currentlyActive = subprocess.Popen("xdotool getactivewindow",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
 #finding pid of MS Teams    
     pid = subprocess.Popen("xdotool search --name '" + name +"'", shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
-#facusing and sending CTRL + SHIFT + M    
+#focusing and sending CTRL + SHIFT + M    
     subprocess.Popen("xdotool windowactivate "+ pid +" key Ctrl+Shift+M", stdout=subprocess.PIPE, shell=True)
 #focusing windows that was previously active    
     subprocess.Popen("xdotool windowactivate '" + currentlyActive + "'", stdout=subprocess.PIPE, shell=True)

21396c0df4e4f95ee8060720cbc24bb05083ac8c
DanValnicek <danvalnicek@gmail.com>
2020-10-28 16:09:05 +0100

    notification when teams has wrong window name

--- teamsMute.py
+++ teamsMute.py
@@ -5,6 +5,8 @@ import subprocess,re
 #finds current name of MS Teams
 name = subprocess.Popen("wmctrl -l| grep 'Microsoft Teams$'|awk '{print substr($0, index($0,$4))  }'",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
 #if it has (Meeting) in name script is run, otherwise we're no in meeting so there is no need to do this
+if str(name) == "Microsoft Teams":
+    subprocess.Popen("notify-send 'RESTART TEAMS teamsMute can not work'", stdout=subprocess.PIPE, shell=True)
 if "(Meeting)" in name:
 #saving pid of the current window
     currentlyActive = subprocess.Popen("xdotool getactivewindow",shell=True, stdout=subprocess.PIPE).communicate()[0].decode('utf-8').strip()
